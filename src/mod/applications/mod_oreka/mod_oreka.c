60decf245d2c6677156261423d96704f411c9c34
Moises Silva <moises.silva@gmail.com>
2012-03-15 00:52:30 -0400

    mod_oreka: Add code to create udp socket on startup and destroy it on close

--- src/mod/applications/mod_oreka/mod_oreka.c
+++ src/mod/applications/mod_oreka/mod_oreka.c
@@ -41,6 +41,11 @@ typedef struct oreka_session_s {
 	switch_core_session_t *session;
 } oreka_session_t;
 
+struct {
+	switch_sockaddr_t *sip_addr;
+	switch_socket_t *sip_socket;
+} globals;
+
 static switch_bool_t oreka_callback(switch_media_bug_t *bug, void *user_data, switch_abc_type_t type)
 {
 	oreka_session_t *oreka = user_data;
@@ -75,7 +80,6 @@ SWITCH_STANDARD_APP(oreka_start_function)
 	char *argv[6];
 	int argc;
 	char *lbuf = NULL;
-	int x = 0;
 
 	if ((bug = (switch_media_bug_t *) switch_channel_get_private(channel, "_oreka_"))) {
 		if (!zstr(data) && !strcasecmp(data, "stop")) {
@@ -93,9 +97,11 @@ SWITCH_STANDARD_APP(oreka_start_function)
 
 	if (data && (lbuf = switch_core_session_strdup(session, data))
 		&& (argc = switch_separate_string(lbuf, ' ', argv, (sizeof(argv) / sizeof(argv[0]))))) {
-		if (!strncasecmp(argv[x], "read", 4)) {
-		    sth->read = 1;
+#if 0
+		if (!strncasecmp(argv[x], "server", sizeof("server"))) {
+			/* parse server=192.168.1.144 string */
 		}
+#endif
 	}
 
 	oreka->session = session;
@@ -112,19 +118,77 @@ SWITCH_STANDARD_APP(oreka_start_function)
 
 SWITCH_MODULE_LOAD_FUNCTION(mod_oreka_load)
 {
+	switch_status_t status = SWITCH_STATUS_FALSE;
 	switch_application_interface_t *app_interface = NULL;
+	switch_size_t len = 0;
+	switch_size_t ilen = 0;
+	int x = 0;
+	switch_sockaddr_t *from_addr = NULL;
+	char dummy_output[] = "Parangaricutirimicuaro";
+	char dummy_input[sizeof(dummy_output)] = "";
+
+	memset(&globals, 0, sizeof(globals));
+
+	switch_sockaddr_info_get(&globals.sip_addr, "sigchld.sangoma.local", SWITCH_UNSPEC, 5060, 0, pool);
+
+	if (switch_socket_create(&globals.sip_socket, switch_sockaddr_get_family(globals.sip_addr), SOCK_DGRAM, 0, pool) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to create socket!\n");
+		return SWITCH_STATUS_UNLOAD;
+	}
+
+	if (switch_socket_opt_set(globals.sip_socket, SWITCH_SO_REUSEADDR, 1) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to set socket option!\n");
+		return SWITCH_STATUS_UNLOAD;
+	}
+
+	if (switch_socket_bind(globals.sip_socket, globals.sip_addr) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to bind to SIP address: %s!\n", strerror(errno));
+		return SWITCH_STATUS_UNLOAD;
+	}
+
+	len = sizeof(dummy_output);
+#ifndef WIN32
+	switch_socket_opt_set(globals.sip_socket, SWITCH_SO_NONBLOCK, TRUE);
+
+	status = switch_socket_sendto(globals.sip_socket, globals.sip_addr, 0, (void *)dummy_output, &len);
+	if (status != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to send UDP message! (status=%d)\n", status);
+	} else {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_WARNING, "Sent message of length %lu!\n", len);
+	}
+
+	status = switch_sockaddr_create(&from_addr, pool);
+	if (status != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Failed to creat socket address\n");
+	}
+	while (!ilen) {
+		ilen = sizeof(dummy_input);
+		status = switch_socket_recvfrom(from_addr, globals.sip_socket, 0, (void *)dummy_input, &ilen);
+		if (status != SWITCH_STATUS_SUCCESS && status != SWITCH_STATUS_BREAK) {
+			break;
+		}
+
+		if (++x > 1000) {
+			break;
+		}
+
+		switch_cond_next();
+	}
+
+	switch_socket_opt_set(globals.sip_socket, SWITCH_SO_NONBLOCK, FALSE);
+#endif
 
 	*module_interface = switch_loadable_module_create_module_interface(pool, modname);
 
 	SWITCH_ADD_APP(app_interface, "oreka_record", "Send media to Oreka recording server", "Send media to Oreka recording server", 
-	oreka_start_function, "[stop]", SAF_NONE);
-
+	oreka_start_function, "[stop]", SAF_NONE); 
 	return SWITCH_STATUS_SUCCESS;
 }
 
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_oreka_shutdown)
 {
-    return SWITCH_STATUS_UNLOAD;
+	switch_socket_close(globals.sip_socket);
+	return SWITCH_STATUS_UNLOAD;
 }
 
 /* For Emacs:
