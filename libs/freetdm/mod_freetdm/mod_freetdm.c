ab444d2448bb9b78b614900420a75a2e839d3a1f
Moises Silva <moises.silva@gmail.com>
2012-09-19 20:02:30 -0400

    freetdm: Fix E&M answer procedure for DAHDI

--- libs/freetdm/mod_freetdm/mod_freetdm.c
+++ libs/freetdm/mod_freetdm/mod_freetdm.c
@@ -2057,6 +2057,7 @@ static FIO_SIGNAL_CB_FUNCTION(on_fxo_signal)
 		}
 		break;
 	case FTDM_SIGEVENT_SIGSTATUS_CHANGED:
+	case FTDM_SIGEVENT_COLLECTED_DIGIT: /* Analog E&M */
 		break;
 	default:
 		{
