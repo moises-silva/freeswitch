23353bfe3826889139455462e7db61e6545f2911
Moises Silva <moy@sangoma.com>
2012-10-11 02:17:27 -0500

    Added support for recording session files in a tmp file first before moving them to their final destination on call end

--- src/include/switch_types.h
+++ src/include/switch_types.h
@@ -1508,7 +1508,8 @@ typedef enum {
 	SWITCH_FILE_DONE = (1 << 13),
 	SWITCH_FILE_BUFFER_DONE = (1 << 14),
 	SWITCH_FILE_WRITE_APPEND = (1 << 15),
-	SWITCH_FILE_WRITE_OVER = (1 << 16)
+	SWITCH_FILE_WRITE_OVER = (1 << 16),
+	SWITCH_FILE_PATH_TMP_EXT = (1 << 17),
 } switch_file_flag_enum_t;
 typedef uint32_t switch_file_flag_t;
 
